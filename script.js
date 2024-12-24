f4b8d252f890c0a92ec667eda2546c5308c46ba8
OsamuChuuya <osamuchuuya26@gmail.com>
2024-12-24 18:14:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let name2 = new Object();
+name2.name1 = prompt("Ism")
+name2.id = prompt('Space Id')
+name2.pass = prompt(`Space Password`)
+console.log('Name; ' + name2.name1)
+console.log(`iD;  `+ name2.id)
+console.log("Password;  " + name2.pass)
